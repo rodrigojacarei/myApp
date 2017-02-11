7dc38b1474a9cc123f495e3ed69a12c597001228
Ubuntu <rodrigo@rodrigojacarei.dwvvfnooio0ujixoep0eg5qfse.bx.internal.cloudapp.net>
2017-02-11 01:06:00 +0000

    Add get request to facebook api

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Facebook, NativeStorage } from 'ionic-native';
 import { LoginPage } from '../login/login';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'page-user',
@@ -12,8 +15,10 @@ export class UserPage {
 
   user: any;
   userReady: boolean = false;
+  status: boolean = false;
+  posts: any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private http:Http) {}
 
   ionViewCanEnter(){
     let env = this;
@@ -39,6 +44,29 @@ export class UserPage {
       nav.push(LoginPage);
     }, function(error){
       console.log(error);
+    });
+  }
+
+  doFbCheckStatus(){
+    var nav = this.navCtrl;
+    let env = this;
+    Facebook.getLoginStatus()
+    .then(function(response) {
+      console.log(response);
+      env.status = true;
+    }, function(error){
+      console.log(error);
+      env.status = false;
+    });
+  }
+
+    makePost() {
+        let env = this;
+        //var url = 'https://randomuser.me/api/';
+        var url = 'https://graph.facebook.com/v2.8/146029542437567/feed?access_token=EAACEdEose0cBAPPYiXvpNVwwiJ8OnBLI7VqkHpMbM9kquM6ek7ZC7IsZAWYH4tk6sXEss0nHWFI14fN9VUZBFbe59EaIaZA54wLnvcAdbEvHPQxijbA2fLLXleaSfQGpez2oO7PWXnKom3ulHasfkFEnZCFDDmSmcc0qH9EzQDbxywZCDH6Y2un8hvou3DQf0ZD&debug=all&format=json&method=get&pretty=0&suppress_http_code=1'
+        var response = this.http.get(url).map(res => res.json()).subscribe(data => {
+      console.log(data);
+	env.posts = data;
     });
   }
 }
